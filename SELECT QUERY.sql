8cd75fbbe02775fedf7f17247e0ca81a28dc9b7e
JGastaud <164484425+JGastaud@users.noreply.github.com>
2026-05-26 19:05:31 +0200

    .

---
+++ SELECT QUERY.sql
@@ -0,0 +1,16 @@
+SELECT *
+from [domosstaging].[company]
+
+SELECT *
+from [domosstaging].[individual]
+where id is null
+
+Select *
+FROM [DomosStaging].[DataRoomload]
+
+SELECT [Investor]
+      ,[InvestorContacts]
+      ,[Fund]
+      ,[FundShare]
+      ,[Amount]
+  FROM [DomosStaging].[OnboardingLoad]
